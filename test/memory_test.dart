7293ce0477bb17464aa107b964229845fa51770d
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2024-03-10 18:53:29 +0100

    feat(memory): add snowflake in tests

--- test/memory_test.dart
+++ test/memory_test.dart
@@ -1,38 +1,39 @@
+import 'package:cache/providers/memory/memory_provider.dart';
 import 'package:test/test.dart';
-import 'package:mineral_cache/providers/memory/memory_provider.dart';
+import 'package:mineral/api/common/snowflake.dart';
 
 void main() {
   test('can create in-memory provider', () {
-    final provider = MemoryProviderContract();
+    final provider = MemoryProvider();
     expect(provider.name, 'InMemoryProvider');
   });
 
   test('can get elements', () {
-    final provider = MemoryProviderContract();
+    final provider = MemoryProvider();
     expect(provider.getAll(), hasLength(0));
   });
 
   test('can put elements', () {
-    final provider = MemoryProviderContract();
-    provider.put('key', 'value');
-    expect(provider.has('key'), true);
+    final provider = MemoryProvider();
+    provider.put(Snowflake('key'), 'value');
+    expect(provider.has(Snowflake('key')), true);
   });
 
   test('can remove elements', () {
-    final provider = MemoryProviderContract();
-    provider.put('key', 'value');
-    provider.remove('key');
-    expect(provider.has('key'), false);
+    final provider = MemoryProvider();
+    provider.put(Snowflake('key'), 'value');
+    provider.remove(Snowflake('key'));
+    expect(provider.has(Snowflake('key')), false);
   });
 
   test('can remove many elements', () {
-    final provider = MemoryProviderContract();
-    provider.put('key', 'value');
-    provider.put('key2', 'value2');
+    final provider = MemoryProvider();
+    provider.put(Snowflake('key1'), 'value');
+    provider.put(Snowflake('key2'), 'value2');
 
-    provider.removeMany(['key', 'key2']);
+    provider.removeMany([Snowflake('key1'), Snowflake('key2')]);
 
-    expect(provider.has('key'), false);
-    expect(provider.has('key2'), false);
+    expect(provider.has(Snowflake('key1')), false);
+    expect(provider.has(Snowflake('key2')), false);
   });
 }

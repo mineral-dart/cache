6383eae1a11928f046ab3bfb135173df5a42efa3
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2024-05-10 00:02:37 +0200

    feat(cache): move string to snowflake contract

--- test/memory_test.dart
+++ test/memory_test.dart
@@ -1,6 +1,8 @@
 import 'package:mineral_cache/providers/memory/memory_provider.dart';
 import 'package:test/test.dart';
 
+import 'package:mineral/api/common/snowflake.dart';
+
 void main() {
   test('can create in-memory provider', () {
     final provider = MemoryProvider();
@@ -14,25 +16,25 @@ void main() {
 
   test('can put elements', () {
     final provider = MemoryProvider();
-    provider.put('key', 'value');
-    expect(provider.has('key'), true);
+    provider.put(Snowflake('key'), 'value');
+    expect(provider.has(Snowflake('key')), true);
   });
 
   test('can remove elements', () {
     final provider = MemoryProvider();
-    provider.put('key', 'value');
-    provider.remove('key');
-    expect(provider.has('key'), false);
+    provider.put(Snowflake('key'), 'value');
+    provider.remove(Snowflake('key'));
+    expect(provider.has(Snowflake('key')), false);
   });
 
   test('can remove many elements', () {
     final provider = MemoryProvider();
-    provider.put('key1', 'value');
-    provider.put('key2', 'value2');
+    provider.put(Snowflake('key1'), 'value');
+    provider.put(Snowflake('key2'), 'value2');
 
-    provider.removeMany(['key1', 'key2']);
+    provider.removeMany([Snowflake('key1'), Snowflake('key2')]);
 
-    expect(provider.has('key1'), false);
-    expect(provider.has('key2'), false);
+    expect(provider.has(Snowflake('key1')), false);
+    expect(provider.has(Snowflake('key2')), false);
   });
 }
